050753c8a6efe9ea0113a2479d70f7241ffeba77
tzurielw <tzubielo@gmail.com>
2024-08-05 12:40:25 +0300

    remove wrong

--- kucoin/kucoin-api-token.py
+++
@@ -1,3 +0,0 @@
-def main():
-     kucoin_api_token = "ed6fb6e18e29fde78618e8ad"
-return kucoin_api_token
--- zendesk/zendesc-secret-key.py
+++
@@ -1,3 +0,0 @@
-def main():
-    zendesk_api_token = "igjs68r72xso86frli7upw1c0udpzuno6n3yumvt"
-return zendesk_api_token
