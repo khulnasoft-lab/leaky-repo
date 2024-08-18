de7168db2fc96fffe0f9283b0aea201ab33916f2
‪Tzuriel Weisberg‬‏ <tzubielo@gmail.com>
2024-08-18 22:53:07 +0300

    Update yandex.py

--- yandex/yandex.py
+++ yandex/yandex.py
@@ -1,5 +1,1 @@
-def main():
-    yandex-aws-access-token =  "YCVdheub7w9bImcGAnd3dZnf08FRbvjeUFvehGvc"
-    yandex-api-key-data = "dict.1.1.20240723T202603Z.2ff3117e15841483.6acf11a604a526e8c0ff8dc1ce59764503809e05"
-    yandex-access-token = "ea135929105c4f29a0f5117d2960926f"
-return yandex-aws-access-token, yandex-api-key-data, yandex-access-token
+
